8be052a4da58d813382be5287cfb450f0e0d7f66
chaeyeon <ccyy1029@naver.com>
2024-05-12 16:16:00 +0900

    🎉 init: eslint, prettier 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,41 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'airbnb',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react', 'react-hooks', 'react-refresh', '@typescript-eslint'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+	},
+
+	overrides: [
+		{
+			files: '**/*.+(ts|tsx)',
+			parser: '@typescript-eslint/parser',
+			plugins: ['@typescript-eslint'],
+			extends: ['plugin:@typescript-eslint/recommended'],
+		},
+		{
+			files: ['**/__tests__/**/*', '**/*.{spec,test}.*'],
+			env: {
+				'jest/globals': true,
+			},
+			plugins: ['jest', 'jest-dom', 'testing-library'],
+			extends: [
+				'plugin:jest/recommended',
+				'plugin:jest-dom/recommended',
+				'plugin:testing-library/react',
+			],
+		},
+	],
+
+	'eslint.validate': ['javascript', 'javascriptreact', 'typescript', 'typescriptreact'],
+};
