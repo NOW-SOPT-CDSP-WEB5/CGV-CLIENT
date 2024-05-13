91a0906635ada5eea7a2b8244261393235ffeea9
wrryu09 <rryu09@ewhain.net>
2024-05-13 20:27:09 +0900

    ✨ home banner 퍼블리싱

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Banner from '../components/home/Banner';
 import NavBar from '../components/home/NavBar';
 import TapBar from '../components/home/TapBar';
 import TopBar from '../components/home/TopBar';
@@ -7,6 +8,7 @@ function Home() {
 		<>
 			<TopBar />
 			<NavBar />
+			<Banner />
 			<TapBar />
 		</>
 	);
