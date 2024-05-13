725aad8863bbccdd544ef6c88672780ddf26186d
wrryu09 <rryu09@ewhain.net>
2024-05-13 20:26:55 +0900

    ✨ feat: banner 이미지 추가

--- src/assets/home/images/index.ts
+++ src/assets/home/images/index.ts
@@ -1,7 +1,14 @@
 import imgHomeLogo from './img_home_logo.png';
+import banner1 from './banner/img_home_banner1.png';
+import banner2 from './banner/img_home_banner2.png';
+import banner3 from './banner/img_home_banner3.png';
+import banner4 from './banner/img_home_banner4.png';
+import banner5 from './banner/img_home_banner5.png';
+import banner6 from './banner/img_home_banner6.png';
 
 const homeImages = {
 	imgHomeLogo,
+	banner: { banner1, banner2, banner3, banner4, banner5, banner6 },
 };
 
 export default homeImages;
