38b461f5631db4a472c9edc0ce5a61c07a6ac338
Jieun Lee <najee0610@naver.com>
2024-05-13 23:24:08 +0900

    ✨ feat: time 이미지 에셋 추가

---
+++ src/assets/time/images/index.ts
@@ -0,0 +1,21 @@
+import imgTimeMovie from './img_time_movie.svg';
+import imgTimePosterMini from './img_time_poster_mini.svg';
+import imgTimeSemicircle from './img_time_semicircle.svg';
+import imgTimetheater from './img_time_theater.svg';
+import imgTimemodalCancel from './img_timemodal_cancel.svg';
+import imgTimemodalComplete from './img_timemodal_complete.svg';
+import imgTimemodalSeat from './img_timemodal_seat.svg';
+
+const timeImages = {
+	imgTimeMovie,
+	imgTimePosterMini,
+	imgTimeSemicircle,
+	imgTimetheater,
+	timeModal: {
+		imgTimemodalCancel,
+		imgTimemodalComplete,
+		imgTimemodalSeat,
+	},
+};
+
+export default timeImages;
