c002405373a715499a966ab3a02b3b465d3f41c7
chaeyeon <ccyy1029@naver.com>
2024-05-14 04:52:05 +0900

    ✨ feat: BottomBar 만들기

--- src/assets/movie/icon/index.ts
+++ src/assets/movie/icon/index.ts
@@ -5,6 +5,9 @@ import icMovie4dx from './ic_movie_4dx.svg';
 import icMovieEgg from './ic_movie_egg.svg';
 import icMovieTicket from './ic_movie_ticket.svg';
 import icMovieGoldenEgg from './ic_movie_goldenegg.svg';
+import icBtnTicketing from './btn_fab_ticketing.svg';
+import icBtnHeartDisabled from './btn_fab_heart_disabled.svg';
+import icBtnShare from './btn_fab_share.svg';
 
 const movieIcons = {
 	tapBar: {
@@ -19,6 +22,11 @@ const movieIcons = {
 		icMovieEgg,
 		icMovieTicket,
 		icMovieGoldenEgg,
+	},
+	BottomBar: {
+		icBtnTicketing,
+		icBtnHeartDisabled,
+		icBtnShare,
 	},
 };
 
