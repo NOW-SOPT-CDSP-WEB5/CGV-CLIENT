67f1d606580fdc9aa18e5d1e26f6d3e492a77b3c
Jieun Lee <najee0610@naver.com>
2024-05-13 23:53:12 +0900

    ✨ feat: 공통 아이콘 에셋 추가

---
+++ src/assets/common/icon/index.ts
@@ -0,0 +1,15 @@
+import icArrowLeft from './ic_arrow_left.svg';
+import icEclipse from './ic_eclipse.svg';
+import icPutterArrow from './ic_putter_arrow.svg';
+import icPutterLine from './ic_putter_line.svg';
+import icX from './ic_x.svg';
+
+const commonIcons = {
+	icArrowLeft,
+	icEclipse,
+	icPutterArrow,
+	icPutterLine,
+	icX,
+};
+
+export default commonIcons;
