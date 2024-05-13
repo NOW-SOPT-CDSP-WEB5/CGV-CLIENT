18762d5063123a20847ff3ee71d416f8096be0a6
chaeyeon <ccyy1029@naver.com>
2024-05-13 14:02:43 +0900

    💄 style: GlobalStyle 전체 width 값 추가

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -35,6 +35,11 @@ footer, header, hgroup, menu, nav, section {
 	display: block;
 }
 
+/* 전체 배경 색상 설정 */
+html, body {
+    background-color: #e0e0e0;
+}
+
 body {
 	line-height: 1;
 }
@@ -57,6 +62,15 @@ table {
 	border-collapse: collapse;
 	border-spacing: 0;
 }
+
+body {
+    width: 375px; /* 모바일 가로 사이즈 375 고정 */
+	min-height: 670px; /* 모바일 세로 사이스 최소 670px 설정 */
+    margin: 0 auto; /* 중앙 정렬 */
+    overflow-x: hidden; /* 가로 스크롤 방지 */
+	background-color: #fff; /* body 영역을 하얀색으로 설정 */
+  }
+
 `;
 
 export default GlobalStyles;
