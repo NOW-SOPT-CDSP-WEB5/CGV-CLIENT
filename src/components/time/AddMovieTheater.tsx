f4cb7e52ab85b2217b0a75a70321c773c0b7bf63
Jieun Lee <najee0610@naver.com>
2024-05-14 23:05:09 +0900

    🐛 fix: 영화/극장 선택 버튼 간격 조정

--- src/components/time/AddMovieTheater.tsx
+++ src/components/time/AddMovieTheater.tsx
@@ -7,41 +7,49 @@ function AddMovieTheater() {
 	return (
 		<>
 			<BackSemiCircleImg src={timeImages.imgTimeSemicircle} alt="semi circle" />
-			<AddMovieTheaterWrapper>
-				<>
+			<AddMovieTheaterContainer>
+				<AddMovieTheaterWrapper>
 					<AddMovieTheaterImg src={timeImages.imgTimeMovie} alt="add movie button" />
 					<MovieChangeIcon src={timeIcon.icTimeChange} alt="change" />
-				</>
+				</AddMovieTheaterWrapper>
 				<AddMovieTheaterButton>
 					<AddMovieTheaterImg src={timeButtons.BtnTimeAddmovie} alt="add movie button" />
 				</AddMovieTheaterButton>
 				<AddMovieTheaterButton>
 					<AddMovieTheaterImg src={timeButtons.BtnTimeAddmovie} alt="add movie button" />
 				</AddMovieTheaterButton>
-				<>
+				<AddMovieTheaterWrapper>
 					<AddMovieTheaterImg src={timeImages.imgTimetheater} alt="add movie theater" />
 					<TheaterName>강남</TheaterName>
 					<TheaterChangeIcon src={timeIcon.icTimeChange} alt="change" />
-				</>
+				</AddMovieTheaterWrapper>
 				<AddMovieTheaterButton>
 					<AddMovieTheaterImg src={timeButtons.BtnTimeAddtheater} alt="add movie theater" />
 				</AddMovieTheaterButton>
-			</AddMovieTheaterWrapper>
+			</AddMovieTheaterContainer>
 		</>
 	);
 }
-const AddMovieTheaterWrapper = styled.div`
+const AddMovieTheaterContainer = styled.div`
 	display: flex;
 	justify-content: space-between;
-	margin: 4rem 1rem 0 0.5rem;
+	margin: 4rem 1.2rem 0;
 `;
 
+const AddMovieTheaterWrapper = styled.div`
+	position: relative;
+`;
 const AddMovieTheaterButton = styled.div`
 	z-index: 1;
 	padding: 0 0.2rem;
+
+	cursor: pointer;
 `;
 const AddMovieTheaterImg = styled.img`
 	z-index: 1;
+
+	cursor: pointer;
+	filter: drop-shadow(0 0.4rem 0.4rem rgb(0 0 0 / 13%));
 `;
 
 const BackSemiCircleImg = styled.img`
@@ -51,23 +59,24 @@ const BackSemiCircleImg = styled.img`
 
 const MovieChangeIcon = styled.img`
 	position: absolute;
-	top: 14.5rem;
-	left: 11rem;
+	top: 6rem;
+	right: 0.5rem;
 	z-index: 2;
 `;
 const TheaterName = styled.p`
 	position: absolute;
-	top: 9.5rem;
-	right: 17.5rem;
+	top: 1rem;
+	left: 1rem;
 	z-index: 2;
+	width: 1.9rem;
 
 	color: ${({ theme }) => theme.Transparent.White75};
 `;
 
 const TheaterChangeIcon = styled.img`
 	position: absolute;
-	top: 14.5rem;
-	right: 14rem;
+	top: 6rem;
+	right: 0.5rem;
 	z-index: 2;
 `;
 export default AddMovieTheater;
