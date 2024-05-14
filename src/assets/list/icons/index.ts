b62df4c43aff6abbfa0f023f100020e56959352a
wrryu09 <rryu09@ewhain.net>
2024-05-15 00:55:57 +0900

    ✨ feat: 필요한 아이콘 추가

---
+++ src/assets/list/icons/index.ts
@@ -0,0 +1,11 @@
+import icListEclipse from './ic_eclipse.svg';
+import icListEclipseMG from './ic_eclipse_MG.svg';
+import icListCheck from './ic_list_check.svg';
+
+const listIcons = {
+	icListEclipse,
+	icListEclipseMG,
+	icListCheck,
+};
+
+export default listIcons;
