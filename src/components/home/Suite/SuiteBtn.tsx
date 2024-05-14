ba4df5e5f724a4fcdcd0c0820d944519f49d7454
wrryu09 <rryu09@ewhain.net>
2024-05-14 13:48:59 +0900

    ✨ feat: SuiteBtn 화살표 추가

--- src/components/home/Suite/SuiteBtn.tsx
+++ src/components/home/Suite/SuiteBtn.tsx
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
+import homeIcons from '../../../assets/home/icon';
 
 function SuiteBtn() {
-	return <SuiteEntireBtn>특별관 전체보기</SuiteEntireBtn>;
+	return (
+		<SuiteEntireBtn>
+			특별관 전체보기
+			<img src={homeIcons.icHomeArrow} alt="arrow" />
+		</SuiteEntireBtn>
+	);
 }
 const SuiteEntireBtn = styled.button`
 	display: flex;
+	gap: 1.1rem;
 	align-items: center;
 	align-self: center;
 	justify-content: center;
 	width: 18.9rem;
 	height: 3.4rem;
 
-	color: ${({ theme }) => theme.GreyScale.LG2};
+	color: ${({ theme }) => theme.GreyScale.MG};
 
 	background-color: ${({ theme }) => theme.GreyScale.White};
 	border: solid 1px ${({ theme }) => theme.GreyScale.LG2};
