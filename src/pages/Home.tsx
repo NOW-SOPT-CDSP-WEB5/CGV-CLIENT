5189b2f6de48874e336b8cea61061ba190faf03e
wrryu09 <rryu09@ewhain.net>
2024-05-14 12:03:26 +0900

    ✨ feat: home 페이지 추천영화 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import Banner from '../components/home/Banner';
 import MovieChart from '../components/home/MovieChart';
 import NavBar from '../components/home/NavBar';
+import PartitionBar from '../components/home/PartitionBar';
+import RecommendSection from '../components/home/RecommendSection';
 import TapBar from '../components/home/TapBar';
 import TopBar from '../components/home/TopBar';
 
@@ -11,6 +13,8 @@ function Home() {
 			<NavBar />
 			<Banner />
 			<MovieChart />
+			<RecommendSection />
+			<PartitionBar />
 			<TapBar />
 		</>
 	);
