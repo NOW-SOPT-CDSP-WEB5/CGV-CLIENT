59dd52623a4295deb8a9f27f3975efc1acbb7334
wrryu09 <rryu09@ewhain.net>
2024-05-15 02:00:17 +0900

    ✨ feat movie에 필요한 에셋 추가

--- src/assets/list/icons/index.ts
+++ src/assets/list/icons/index.ts
@@ -2,10 +2,24 @@ import icListEclipse from './ic_eclipse.svg';
 import icListEclipseMG from './ic_eclipse_MG.svg';
 import icListCheck from './ic_list_check.svg';
 
+// movie
+import icListAge from './ic_home_age.png';
+
+// tags
+import icMovieTypeIMAX from './ic_list_movietype_IMAX.png';
+import icMovieType4DX from './ic_list_movietype_4DX.png';
+
 const listIcons = {
 	icListEclipse,
 	icListEclipseMG,
 	icListCheck,
+	movie: {
+		icListAge,
+		tags: {
+			icMovieTypeIMAX,
+			icMovieType4DX,
+		},
+	},
 };
 
 export default listIcons;
