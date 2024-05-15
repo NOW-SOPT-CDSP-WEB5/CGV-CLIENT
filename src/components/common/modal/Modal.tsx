490d49a9d20c6de6474e6b371f458d29bcf5d58e
Jieun Lee <najee0610@naver.com>
2024-05-15 19:04:28 +0900

    ✨ feat: movie 예매 취소 모달 퍼블리싱

---
+++ src/components/common/modal/Modal.tsx
@@ -0,0 +1,59 @@
+import React, { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+interface ModalProps {
+	onClickToggleModal: () => void;
+}
+
+/** 모달창 프레임 함수 */
+function Modal({ onClickToggleModal, children }: PropsWithChildren<ModalProps>) {
+	return (
+		<ModalContainer>
+			<DialogBox>{children}</DialogBox>
+			<Backdrop
+				onClick={(e: React.MouseEvent) => {
+					e.preventDefault();
+					onClickToggleModal();
+				}}
+			/>
+		</ModalContainer>
+	);
+}
+
+const ModalContainer = styled.div`
+	position: fixed;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: 100%;
+`;
+
+const DialogBox = styled.dialog`
+	z-index: 10000;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	box-sizing: border-box;
+	width: 29.4rem;
+	height: 17.5rem;
+
+	background-color: white;
+	box-shadow: 0 0 30px rgb(30 30 30 / 18.5%);
+	border: none;
+	border-radius: 8px;
+`;
+
+const Backdrop = styled.div`
+	position: fixed;
+	top: 0;
+	z-index: 9999;
+
+	width: 100vw;
+	height: 100vh;
+
+	background-color: ${({ theme }) => theme.Transparent.Black75};
+`;
+
+export default Modal;
---
+++ src/components/movie/modal/CancelModal.tsx
@@ -0,0 +1,76 @@
+import React, { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+import Modal from '../../common/modal/Modal';
+import Typo from '../../../styles/typo/typo';
+
+interface CancelModalProps {
+	onClickToggleModal: () => void;
+}
+
+/** 예매취소 모달 내용 */
+function CancelModal({ onClickToggleModal, children }: PropsWithChildren<CancelModalProps>) {
+	return (
+		<Modal onClickToggleModal={onClickToggleModal}>
+			<CancelTitle>
+				예매한 &lt;범죄도시4&gt;
+				<br /> 영화를 취소하시겠어요?
+			</CancelTitle>
+			<Theater>강남 CGV 1관 6층</Theater>
+			<Typo.Title.Title6B18>07:40 ~ 09:39</Typo.Title.Title6B18>
+			<CancelMsg>취소 후 되돌릴 수 없으니 신중하게 선택해주세요</CancelMsg>
+			{children}
+			<ButtonWrapper>
+				<CloseButton type="button" onClick={onClickToggleModal}>
+					<Typo.Head.Head1SB17>닫기</Typo.Head.Head1SB17>
+				</CloseButton>
+
+				<CancelButton type="button">
+					<Typo.Head.Head1SB17>예매 취소하기</Typo.Head.Head1SB17>
+				</CancelButton>
+			</ButtonWrapper>
+		</Modal>
+	);
+}
+
+const CancelButton = styled.button`
+	width: 14.7rem;
+	height: 5.2rem;
+
+	color: ${({ theme }) => theme.GreyScale.White};
+
+	background-color: ${({ theme }) => theme.Color.Point};
+	border: none;
+	border-bottom-right-radius: 8px;
+`;
+const CloseButton = styled.button`
+	width: 14.7rem;
+	height: 5.2rem;
+
+	color: ${({ theme }) => theme.GreyScale.White};
+
+	background-color: ${({ theme }) => theme.GreyScale.MG};
+	border: none;
+	border-bottom-left-radius: 8px;
+`;
+
+const CancelTitle = styled(Typo.Head.Head1SB17)`
+	padding-bottom: 1rem;
+
+	text-align: center;
+`;
+
+const ButtonWrapper = styled.div`
+	position: fixed;
+	bottom: 29rem;
+	display: flex;
+`;
+const Theater = styled(Typo.Title.Title3SB14)`
+	color: ${({ theme }) => theme.Color.Point};
+`;
+const CancelMsg = styled(Typo.Caption.Caption2R11)`
+	padding-top: 0.8rem;
+
+	color: ${({ theme }) => theme.GreyScale.MG};
+`;
+
+export default CancelModal;
