03ef485b628aefde9f15e73ccc8262b3559a0d96
chaeyeon <ccyy1029@naver.com>
2024-05-16 05:11:58 +0900

    ✨ feat: DetailCharacter 만들기

---
+++ src/assets/movie/images/index.ts
@@ -0,0 +1,17 @@
+import movieCharacterHeo from './img_movie_heo.svg';
+import movieCharacterKim from './img_movie_kim.svg';
+import movieCharacterLee from './img_movie_lee.svg';
+import movieCharacterMa from './img_movie_ma.svg';
+import movieCharacterPark from './img_movie_park.svg';
+
+const movieImages = {
+	movieCharacter: {
+		movieCharacterHeo,
+		movieCharacterKim,
+		movieCharacterLee,
+		movieCharacterMa,
+		movieCharacterPark,
+	},
+};
+
+export default movieImages;
--- src/components/movie/MovieDetail/MovieDetailCharacter.tsx
+++ src/components/movie/MovieDetail/MovieDetailCharacter.tsx
@@ -1,5 +1,41 @@
+import styled from 'styled-components';
+import Typo from '../../../styles/typo/typo';
+import movieImages from '../../../assets/movie/images';
+
 function DetailCharacter() {
-	return <div />;
+	return (
+		<>
+			<Typo.Title.Title1SB16>감독/등장인물</Typo.Title.Title1SB16>
+			<MovieCharacterWapper>
+				<div>
+					<img src={movieImages.movieCharacter.movieCharacterHeo} alt="허명행" />
+					<Typo.Title.Title3SB14>허명행</Typo.Title.Title3SB14>
+					<p>감독</p>
+				</div>
+				<div>
+					<img src={movieImages.movieCharacter.movieCharacterMa} alt="마동석" />
+					<Typo.Title.Title3SB14>마동석</Typo.Title.Title3SB14>
+				</div>
+				<div>
+					<img src={movieImages.movieCharacter.movieCharacterKim} alt="김무열" />
+					<Typo.Title.Title3SB14>김무열</Typo.Title.Title3SB14>
+				</div>
+				<div>
+					<img src={movieImages.movieCharacter.movieCharacterLee} alt="이동휘" />
+					<Typo.Title.Title3SB14>이동휘</Typo.Title.Title3SB14>
+				</div>
+				<div>
+					<img src={movieImages.movieCharacter.movieCharacterPark} alt="박지환" />
+					<Typo.Title.Title3SB14>박지환</Typo.Title.Title3SB14>
+				</div>
+			</MovieCharacterWapper>
+		</>
+	);
 }
+
+const MovieCharacterWapper = styled.div`
+	display: flex;
+	margin-bottom: 4.5rem;
+`;
 
 export default DetailCharacter;
