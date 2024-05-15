697a6042da3c87cfdd5730a9809b5c8141c8490e
wrryu09 <rryu09@ewhain.net>
2024-05-16 02:36:32 +0900

    ✨ feat: GlobalFont 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyle';
 import theme from './styles/theme';
+import GlobalFont from './styles/GlobalFont';
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
+			<GlobalFont />
 			<GlobalStyles />
 			<p>sample</p>
 		</ThemeProvider>
