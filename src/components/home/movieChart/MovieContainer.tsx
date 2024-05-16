f73f224a4a83fdef112f95017abd550a65c4c4b3
wrryu09 <rryu09@ewhain.net>
2024-05-16 17:10:29 +0900

    💄 style: MovieImgContainer 내부 스타일링

--- src/components/home/movieChart/MovieContainer.tsx
+++ src/components/home/movieChart/MovieContainer.tsx
@@ -10,53 +10,20 @@ function MovieContainer() {
 			<MovieBox>
 				<MovieImgContainer>
 					<MovieImg src={homeImages.movie.movie1} alt="범죄도시" />
-					<MovieImgTxtBox>
-						<p>1</p>
-						<p>91.4%</p>
-					</MovieImgTxtBox>
+					<MovieImgTxtWrapper>
+						<AgeImg src={homeIcons.icHomeAge} alt="15세 제한" />
+						<MovieImgTxtBox>
+							<MovieImgTextNumber>1</MovieImgTextNumber>
+							<MovieImgTextDesc>91.4%</MovieImgTextDesc>
+						</MovieImgTxtBox>
+					</MovieImgTxtWrapper>
 				</MovieImgContainer>
 				<Typo.Title.Title1SB16>범죄도시4</Typo.Title.Title1SB16>
 				<MovieDetailContainer>
 					<EggImg src={homeIcons.icHomeEgg} alt="egg" />
-					<p>99%</p>
+					<MGBody5M13>99%</MGBody5M13>
 					<img src={homeIcons.icHomeLine} alt="line" />
-					<p>D-1</p>
-				</MovieDetailContainer>
-				<BookBtn />
-			</MovieBox>
-
-			<MovieBox>
-				<MovieImgContainer>
-					<MovieImg src={homeImages.movie.movie1} alt="범죄도시" />
-					<MovieImgTxtBox>
-						<p>1</p>
-						<p>91.4%</p>
-					</MovieImgTxtBox>
-				</MovieImgContainer>
-				<Typo.Title.Title1SB16>범죄도시4</Typo.Title.Title1SB16>
-				<MovieDetailContainer>
-					<EggImg src={homeIcons.icHomeEgg} alt="egg" />
-					<p>99%</p>
-					<img src={homeIcons.icHomeLine} alt="line" />
-					<p>D-1</p>
-				</MovieDetailContainer>
-				<BookBtn />
-			</MovieBox>
-
-			<MovieBox>
-				<MovieImgContainer>
-					<MovieImg src={homeImages.movie.movie1} alt="범죄도시" />
-					<MovieImgTxtBox>
-						<p>1</p>
-						<p>91.4%</p>
-					</MovieImgTxtBox>
-				</MovieImgContainer>
-				<Typo.Title.Title1SB16>범죄도시4</Typo.Title.Title1SB16>
-				<MovieDetailContainer>
-					<EggImg src={homeIcons.icHomeEgg} alt="egg" />
-					<p>99%</p>
-					<img src={homeIcons.icHomeLine} alt="line" />
-					<p>D-1</p>
+					<PointBody5M13>D-1</PointBody5M13>
 				</MovieDetailContainer>
 				<BookBtn />
 			</MovieBox>
@@ -89,14 +56,34 @@ const MovieImg = styled.img`
 	width: 100%;
 	height: 100%;
 `;
-const MovieImgTxtBox = styled.div`
+const MovieImgTxtWrapper = styled.div`
 	position: absolute;
 	bottom: 0;
 	left: 0;
 	z-index: 1;
-	padding: 0.3rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	width: 100%;
+	height: 100%;
 
 	color: ${({ theme }) => theme.GreyScale.White};
+`;
+const MovieImgTxtBox = styled.div`
+	display: flex;
+	gap: 0.5rem;
+	align-items: baseline;
+`;
+const MovieImgTextNumber = styled.p`
+	color: ${({ theme }) => theme.GreyScale.White};
+	font-weight: 500;
+	font-size: 4.8rem;
+	font-style: italic;
+`;
+const MovieImgTextDesc = styled.p`
+	color: ${({ theme }) => theme.GreyScale.White};
+	font-weight: 400;
+	font-size: 1.4rem;
 `;
 const MovieDetailContainer = styled.div`
 	display: flex;
@@ -108,5 +95,17 @@ const MovieDetailContainer = styled.div`
 const EggImg = styled.img`
 	width: 1.4rem;
 	height: 1.6rem;
+`;
+const AgeImg = styled.img`
+	align-self: flex-end;
+	width: 2rem;
+	height: 2rem;
+	padding: 0.5rem;
+`;
+const MGBody5M13 = styled(Typo.Body.Body5M13)`
+	color: ${({ theme }) => theme.GreyScale.MG};
+`;
+const PointBody5M13 = styled(Typo.Body.Body5M13)`
+	color: ${({ theme }) => theme.Color.Point};
 `;
 export default MovieContainer;
