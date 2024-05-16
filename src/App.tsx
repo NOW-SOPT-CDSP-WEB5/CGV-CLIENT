e8a8648ccdcd86626c63b2be5d2a3d84d4b63b32
chaeyeon <ccyy1029@naver.com>
2024-05-17 02:42:35 +0900

    💄 style: typo title1 font-size 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyle';
 import theme from './styles/theme';
 import GlobalFont from './styles/GlobalFont';
+import Movie from './pages/Movie';
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalFont />
 			<GlobalStyles />
+			<Movie />
 		</ThemeProvider>
 	);
 }
