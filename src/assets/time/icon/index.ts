45b07122c67c4b7f34414409642b9db397949f78
Jieun Lee <najee0610@naver.com>
2024-05-16 23:08:07 +0900

    ✨ feat: time 에셋 추가

--- src/assets/time/icon/index.ts
+++ src/assets/time/icon/index.ts
@@ -3,6 +3,7 @@ import icArrowUp from './ic_arrow_up.svg';
 import icMovieStar from './ic_movie_star.svg';
 import icTimeChange from './ic_time_change.svg';
 import icTimeFilter from './ic_time_filter.svg';
+import icTimeLine from './ic_time_line.svg';
 
 const timeIcons = {
 	icArrowDown,
@@ -10,6 +11,7 @@ const timeIcons = {
 	icMovieStar,
 	icTimeChange,
 	icTimeFilter,
+	icTimeLine,
 };
 
 export default timeIcons;
