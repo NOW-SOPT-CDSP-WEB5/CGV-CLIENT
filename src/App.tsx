7f499d9b8d85bd41ce7242017c2cffdfee2ec6ac
chaeyeon <ccyy1029@naver.com>
2024-05-18 02:51:41 +0900

    ✨ feat: Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyle';
 import theme from './styles/theme';
 import GlobalFont from './styles/GlobalFont';
+import Router from './Router';
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalFont />
 			<GlobalStyles />
+			<Router />
 		</ThemeProvider>
 	);
 }
---
+++ src/Router.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import List from './pages/List';
+
+function Router() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="movie" element={<Movie />} />
+                <Route path="list" element={<List />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default Router;
