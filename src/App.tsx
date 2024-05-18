f3a05a0eb9e487e3ec0c15fa9d361a542ded3ed9
Jieun Lee <najee0610@naver.com>
2024-05-18 14:31:55 +0900

    🐛 fix: 푸터 두번 생성 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyle';
 import theme from './styles/theme';
 import GlobalFont from './styles/GlobalFont';
-import Footer from './components/common/Footer';
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalFont />
 			<GlobalStyles />
-			<Footer/>
 		</ThemeProvider>
 	);
 }
