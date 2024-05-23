169f108fa0679bd4b36d8a5290becdd9ec174dbf
Jieun Lee <najee0610@naver.com>
2024-05-24 02:23:25 +0900

    ✨ feat: 선택한 시간을 예매 모달에 표시하도록 수정

--- src/components/time/modal/BookingModal.tsx
+++ src/components/time/modal/BookingModal.tsx
@@ -3,14 +3,23 @@ import styled from 'styled-components';
 import Modal from '../../common/modal/Modal';
 import Typo from '../../../styles/typo/typo';
 import postMoviesTickets from '../../../apis/postMoviesTickets';
+import formatTime from '../../../utils/time/hourMinuteUtils';
 
 interface BookingModalProps {
 	onClickToggleModal: () => void;
 	onConfirmBooking: () => void;
+	startAt: string;
+	endAt: string;
 }
 
 /** 예매 모달 내용 */
-function BookingModal({ onClickToggleModal, onConfirmBooking, children }: PropsWithChildren<BookingModalProps>) {
+function BookingModal({
+	onClickToggleModal,
+	onConfirmBooking,
+	startAt,
+	endAt,
+	children,
+}: PropsWithChildren<BookingModalProps>) {
 	const handleBookingClick = async () => {
 		await postMoviesTickets();
 		onConfirmBooking(); // 예매 확인 모달 표시
@@ -18,13 +27,15 @@ function BookingModal({ onClickToggleModal, onConfirmBooking, children }: PropsW
 
 	return (
 		<Modal onClickToggleModal={onClickToggleModal}>
-			<BookingModalWrpaper>
+			<BookingModalWrapper>
 				<BookingTitle>범죄도시4</BookingTitle>
 				<Theater>강남 CGV 1관 6층</Theater>
-				<Typo.Title.Title6B18>07:40 ~ 09:39</Typo.Title.Title6B18>
+				<Typo.Title.Title6B18>
+					{formatTime(startAt)} ~ {formatTime(endAt)}
+				</Typo.Title.Title6B18>
 				<BookingMsg>영화 정보와 극장, 상영관과 영화 시간을 확인해주세요</BookingMsg>
 				{children}
-			</BookingModalWrpaper>
+			</BookingModalWrapper>
 			<ButtonWrapper>
 				<CloseButton type="button" onClick={onClickToggleModal}>
 					<Typo.Head.Head1SB17>취소</Typo.Head.Head1SB17>
@@ -37,7 +48,9 @@ function BookingModal({ onClickToggleModal, onConfirmBooking, children }: PropsW
 	);
 }
 
-const BookingModalWrpaper = styled.div`
+export default BookingModal;
+
+const BookingModalWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
@@ -88,5 +101,3 @@ const BookingMsg = styled(Typo.Caption.Caption2R11)`
 
 	color: ${({ theme }) => theme.GreyScale.MG};
 `;
-
-export default BookingModal;
