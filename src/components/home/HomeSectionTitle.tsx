5b16a187e21a97f74e7737284b4d674176ec50c1
wrryu09 <rryu09@ewhain.net>
2024-05-23 16:51:03 +0900

    ✨ feat: list 네비게이션

--- src/components/home/HomeSectionTitle.tsx
+++ src/components/home/HomeSectionTitle.tsx
@@ -2,18 +2,23 @@ import styled from 'styled-components';
 import Typo from '../../styles/typo/typo';
 import OverViewBtn from './OverViewBtn';
 
-type Props = { text: string; main: boolean };
+type Props = { text: string; main: boolean; onClick?: (() => void) | undefined };
 
 /** 섹션제목 */
 function HomeSectionTitle(props: Props) {
-	const { text, main } = props;
+	const { text, main, onClick } = props;
 	return (
 		<SectionTitle>
 			{main ? <Typo.Title.Title6B18>{text}</Typo.Title.Title6B18> : <Typo.Title.Title8B15>{text}</Typo.Title.Title8B15>}
-			<OverViewBtn />
+			<OverViewBtn onClick={onClick} />
 		</SectionTitle>
 	);
 }
+
+HomeSectionTitle.defaultProps = {
+	onClick: undefined,
+};
+
 const SectionTitle = styled.div`
 	display: flex;
 	align-items: center;
--- src/components/home/OverViewBtn.tsx
+++ src/components/home/OverViewBtn.tsx
@@ -3,14 +3,17 @@ import Typo from '../../styles/typo/typo';
 import homeIcons from '../../assets/home/icon';
 
 /** 전체보기 버튼 */
-function OverViewBtn() {
+function OverViewBtn({ onClick }: { onClick?: (() => void) | undefined }) {
 	return (
-		<OverViewBtnContainer>
+		<OverViewBtnContainer onClick={onClick}>
 			<Typo.Body.Body5M13>전체보기</Typo.Body.Body5M13>
 			<img src={homeIcons.icHomeArrow} alt="arrow" />
 		</OverViewBtnContainer>
 	);
 }
+OverViewBtn.defaultProps = {
+	onClick: undefined,
+};
 const OverViewBtnContainer = styled.div`
 	display: flex;
 	gap: 0.6rem;
