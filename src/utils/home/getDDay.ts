289510030fc9019a3210953a68000a6cff1c25f6
wrryu09 <rryu09@ewhain.net>
2024-05-23 17:12:21 +0900

    🐛 fix: getDDay 수정

--- src/utils/home/getDDay.ts
+++ src/utils/home/getDDay.ts
@@ -1,16 +1,16 @@
 /** releaseDate 와 현재 시간 비교해서 D-Day 반환 */
 const getDDay = (releaseDate: string) => {
 	const date = new Date();
-	const releaseDateArr = releaseDate.split('.');
-	const releaseDateFormatted = new Date(`${releaseDateArr[0]}-${releaseDateArr[1]}-${releaseDateArr[2]}`);
+	const [year, month, day] = releaseDate.split('.');
+	const releaseDateFormatted = new Date(`${year}-${month}-${day}`);
 
-	let diff = Math.abs(date.getTime() - releaseDateFormatted.getTime());
+	let diff = date.getTime() - releaseDateFormatted.getTime();
 	diff = Math.ceil(diff / (1000 * 60 * 60 * 24));
 	if (diff > 0) {
 		return `D+${diff}`;
 	}
 	if (diff < 0) {
-		return `D+${Math.abs(diff)}`;
+		return `D${diff}`;
 	}
 	// 같음
 	return 'D-Day';
